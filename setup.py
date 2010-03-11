9b169a1b366596730ea598d84c01e77392a76c5f
Tim Parkin <tim.parkin@jdi-associates.com>
2010-03-11 16:12:44 +0000

    cleaned up, added description

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.1'
+version = '0.1.1'
 
 setup(name='couchutil',
       version=version,
-      description="",
+      description="utils for couchdb. changes handler and reservation handling ctx mgr.",
       long_description="""\
 """,
       classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='',
-      author='Matt Goodall',
-      author_email='matt.goodall@gmail.com',
-      url='',
-      license='BSD',
+      author='Tim Parkin & Matt Goodall',
+      author_email='info@timparkin.co.uk',
+      url='http://ish.io',
+      license='',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
       zip_safe=False,
